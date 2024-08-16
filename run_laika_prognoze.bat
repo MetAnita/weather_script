2c8e9300b630be6ec207fcfdd54664d64cb064e2
Anita Metlova <anitametlova@gmail.com>
2024-08-16 14:06:22 +0300

    added bat file

---
+++ run_laika_prognoze.bat
@@ -0,0 +1,9 @@
+@echo off
+"C:\Users\Admin\PycharmProjects\weather_script\venv\Scripts\python.exe" "C:\Users\Admin\PycharmProjects\weather_script\main.py"
+
+"C:\Users\Admin\PycharmProjects\weather_script\venv\Scripts\python.exe" "C:\Users\Admin\PycharmProjects\weather_script\forecast_yr.py"
+
+"C:\Users\Admin\PycharmProjects\weather_script\venv\Scripts\python.exe" "C:\Users\Admin\PycharmProjects\weather_script\forecast_lvgmc.py"
+
+"C:\Users\Admin\PycharmProjects\weather_script\venv\Scripts\python.exe" "C:\Users\Admin\PycharmProjects\weather_script\write_excel.py"
+pause
